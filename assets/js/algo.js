0b196ad625ad7bb27518a40c17cb12f4b4cc7802
Yskinator <yski2moreletters@gmail.com>
2015-10-02 20:08:36 +0300

    Added distance calculations.

--- assets/js/algo.js
+++ assets/js/algo.js
@@ -23,7 +23,15 @@ MTHONALGO.solveStellarRoute = function(dataset) {
         if(dataset["stars"].hasOwnProperty(key))
         {
             var star = dataset["stars"][key];
-            console.log(star["_id"])
+            for(key2 in dataset["stars"])
+            {
+                if(dataset["stars"].hasOwnProperty(key2))
+                {
+                    var star2 = dataset["stars"][key2];
+                    console.log(distance(star,star2))
+            
+                }
+            }
         }
     }
     // required return
@@ -31,3 +39,26 @@ MTHONALGO.solveStellarRoute = function(dataset) {
 }
 
 // add your support functions in here
+function distance(star1, star2)
+{
+    if(hasPosition(star1) && hasPosition(star2))
+    {
+        star1 = star1["position"]
+        star2 = star2["position"]
+        return Math.sqrt(Math.pow((star1["x"]-star2["x"]),2)+Math.pow((star1["y"]-star2["y"]),2)+Math.pow((star1["z"]-star2["z"]),2))
+    }
+    else
+    {
+        throw new Error("HALP!")
+    }
+}
+
+function hasPosition(star)
+{
+    var pos = star["position"]
+    if(pos.hasOwnProperty("x") && pos.hasOwnProperty("y") && pos.hasOwnProperty("z"))
+    {
+        return true
+    }
+    return false
+}
